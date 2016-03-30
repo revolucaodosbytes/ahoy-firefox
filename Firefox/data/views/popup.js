773789cf4c0ddf44f9742657222c264db6387b6d
Rafael Almeida <rafaelcpalmeida@gmail.com>
2016-03-30 09:55:52 +0100

    Improved code indentation

--- Firefox/data/views/popup.js
+++ Firefox/data/views/popup.js
@@ -1,33 +1,29 @@
 window.addEventListener('click', function(event) {
     var target = event.target;
     
-    if (target.id == 'forcarProxy' || target.id == 'listaSites')
-    {
+    if (target.id == 'forcarProxy' || target.id == 'listaSites') {
         self.port.emit('daNovoProxy', target.toString());
     }
     
-    if (target.id == 'verSites')
-    {
+    if (target.id == 'verSites') {
         self.port.emit('openTabSites', target.toString());
     }
     
-    if (target.id == 'mensagem')
-    {
+    if (target.id == 'mensagem') {
         self.port.emit('openTabLink', target.toString());
     }
 }, false);
 
 self.on('message', function(data){
-    if(data.hasOwnProperty('host') && data.hasOwnProperty('port'))
-    {
+    if(data.hasOwnProperty('host') && data.hasOwnProperty('port')) {
        document.getElementById("proxyaddr").innerHTML = data.host + ':' + data.port;
     }
-    if(data.hasOwnProperty('version'))
-    {
+    
+    if(data.hasOwnProperty('version')) {
         document.getElementById("ahoy-version").innerHTML = data.version;
     }
-    if(data.hasOwnProperty('messageText'))
-    {
+    
+    if(data.hasOwnProperty('messageText')) {
         document.getElementById("mensagem").innerHTML = data.messageText;
     }
 })
@@ -35,12 +31,10 @@ self.on('message', function(data){
 self.port.on('currentURL', function( currentURL, blockedSites ) {  
     var cleanURL = currentURL.replace(/.*?:\/\/www.|.*?:\/\//g,"").replace(/\//g,"");
 
-    if (cleanURL in blockedSites)
-    {
+    if (cleanURL in blockedSites) {
        document.getElementById('status-inactivo').style.display = "none";
        document.getElementById('status-activo').style.display = "";
-    } else 
-    {
+    } else {
        document.getElementById('status-inactivo').style.display = "";
        document.getElementById('status-activo').style.display = "none";    
    }
