fc02c1a304879328d6563a4f6078061fd614ea40
Rafael Almeida <rafaelcpalmeida@gmail.com>
2017-09-15 18:58:53 +0100

    Minor improvements

--- Firefox/ahoy.js
+++ Firefox/ahoy.js
@@ -16,7 +16,7 @@ var Ahoy = function() {
 	this.last_request_redirected = false;
 
 	// Update the info with the latest content from the Local Storage
-	browser.storage.local.get( [ "sites_list", "proxy_addr"],function(result) {
+	browser.storage.local.get( [ "sites_list", "proxy_addr" ],function(result) {
 		if (result.sites_list !== undefined)
 			this.sites_list = result.sites_list;
 
@@ -112,6 +112,65 @@ Ahoy.prototype.update_proxy = function ( forceReload ) {
 	xhr.send();
 };
 
+/**
+ * Callbacks
+ */
+
+Ahoy.prototype.init_callbacks = function( ) {
+	console.log("Initializing callbacks");
+
+	// Setup the callback filters
+	this.setup_callback_filters();
+
+	browser.tabs.onUpdated.addListener(this.update_browse_action_icon.bind(this));
+	browser.webRequest.onResponseStarted.addListener( this.check_for_blocked_site.bind(this), {urls: ["<all_urls>"]} );
+
+	// Stats
+	browser.webNavigation.onCompleted.addListener( this.send_hostname.bind(this), {url: this.webnav_filter_list } );
+};
+
+Ahoy.prototype.update_callbacks = function() {
+	// Remove all the callbacks
+	console.log("Updating old callbacks...");
+
+	browser.tabs.onUpdated.removeListener(this.update_browse_action_icon.bind(this));
+	browser.webRequest.onResponseStarted.removeListener(this.check_for_blocked_site.bind(this));
+
+	// Stats
+	browser.webNavigation.onCompleted.removeListener(this.send_hostname.bind(this));
+
+	// Recreate new callbacks
+	this.init_callbacks();
+}
+
+Ahoy.prototype.update_browse_action_icon = function(tabId, changeInfo, tabInfo) {
+	if( tabInfo.url == undefined ) {
+		return;
+	}
+
+	if ( ! this.is_url_in_list( tabInfo.url ) ) {
+		return;
+	}
+	
+	// Turn the icon red for this tab
+	browser.browserAction.setIcon({
+		path: {
+			"38":  "icons/color/38x38.png",
+		},
+		tabId: tabId,
+	});
+}
+
+
+/**
+ * If the connection fails, for exemple, dead proxy, get a new one
+ */
+Ahoy.prototype.change_proxy_if_connection_fails = function ( details ) {
+	if ( details.error == "net::ERR_PROXY_CONNECTION_FAILED" ) {
+		this.update_proxy();
+	}
+};
+
 Ahoy.prototype.after_update = function( details ) {
 	// Make sure the plugins fetch for new information when it"s installed/updated
 	this.update_site_list();
@@ -177,6 +236,9 @@ Ahoy.prototype.event_sites_updated = function( e ) {
     browser.storage.local.set( { "sites_list": e.detail.sites } );
 
     this.sites_list = e.detail.sites;
+
+  	// Update the old callbacks
+  	this.update_callbacks();
 };
 
 Ahoy.prototype.check_for_blocked_redirected_site = function( details ) {
