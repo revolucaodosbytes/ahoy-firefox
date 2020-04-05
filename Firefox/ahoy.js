1d1afb465f0daf098e2fadf6f48ff788d9ca8e54
Henrique Mouta <henrique@automattic.com>
2020-04-05 15:19:11 +0100

    Revert "Deprecated functions have been replaced"

--- Firefox/ahoy.js
+++ Firefox/ahoy.js
@@ -1,114 +1,112 @@
+
 var Ahoy = function() {
 	/**
 	 * CONFIGS
 	 */
-	this.api_url = 'https://ahoy-api.revolucaodosbytes.pt';
+	this.api_url = "https://ahoy-api.revolucaodosbytes.pt";
 
 	/**
 	 * DEFAULTS
 	 */
-	this.sites_list = ['thepiratebay.org'];
-	this.proxy_addr = 'proxy1.ahoy.pro:3127'; //default proxy
+	this.sites_list = ["thepiratebay.org"];
+	this.proxy_addr = "proxy1.ahoy.pro:3127"; //default proxy
 	this.webreq_filter_list = [];
 	this.webnav_filter_list = [];
 
 	this.last_request_redirected = false;
 
 	// Update the info with the latest content from the Local Storage
-	browser.storage.local.get(
-		['sites_list', 'proxy_addr'],
-		function(result) {
-			if (result.sites_list !== undefined) this.sites_list = result.sites_list;
+	browser.storage.local.get( [ "sites_list", "proxy_addr" ],function(result) {
+		if (result.sites_list !== undefined)
+			this.sites_list = result.sites_list;
 
-			if (result.proxy_addr !== undefined) this.proxy_addr = result.proxy_addr;
+		if (result.proxy_addr !== undefined)
+			this.proxy_addr = result.proxy_addr;
 
-			this.enable_proxy();
+		this.enable_proxy();
 
-			// Init
-			this.init_events();
-		}.bind(this)
-	);
+		// Init
+		this.init_events();
 
-	browser.runtime.onInstalled.addListener(this.after_update.bind(this));
+	}.bind(this));
+
+	browser.runtime.onInstalled.addListener( this.after_update.bind(this) );
 };
 
-Ahoy.prototype.enable_proxy = function() {
+Ahoy.prototype.enable_proxy = function () {
 	// Mudar o proxy
 	var config = {
-		mode: 'pac_script',
-		pacScript: {
-			url: this.api_url + '/api/pac?proxy_addr=' + this.proxy_addr,
-			mandatory: false
-		}
-	};
-
+	    mode: "pac_script",
+	    pacScript: {
+	      url: this.api_url + "/api/pac?proxy_addr=" + this.proxy_addr,
+	      mandatory: false
+	    }
+	  };
+	  
 	// Describes the current proxy setting being used.
 	var proxySettings = {
-		value: config,
-		scope: 'regular'
+		"value": config,
+		"scope": "regular"
 	};
 
 	// Setup new settings for the appropriate window.
-	console.log('Applying proxy settings for ' + this.proxy_addr);
+	console.log("Applying proxy settings for " + this.proxy_addr);
 	//browser.proxy.settings.set(proxySettings);
 };
 
 /**
  * Retore PAC callback
  */
-Ahoy.prototype.disable_proxy = function() {
-	console.log('Reverting proxy settings');
-	browser.proxy.settings.clear({ scope: 'regular' });
+Ahoy.prototype.disable_proxy = function( ) {
+	console.log( "Reverting proxy settings");
+	browser.proxy.settings.clear( { scope: "regular" } );
 };
 
-Ahoy.prototype.update_site_list = function() {
+Ahoy.prototype.update_site_list = function () { 
 	var xhr = new XMLHttpRequest();
 
-	xhr.open('GET', this.api_url + '/api/sites');
+	xhr.open("GET", this.api_url + "/api/sites" );
 
 	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-			console.log('Site list sucessfully retrived.');
-			// JSON.parse does not evaluate the attacker's scripts.
-			var resp = JSON.parse(xhr.responseText);
+	  if (xhr.readyState == 4) {
+	 	console.log("Site list sucessfully retrived.");
+	    // JSON.parse does not evaluate the attacker's scripts.
+	    var resp = JSON.parse(xhr.responseText);
 
-			// Dispatch the event
-			document.dispatchEvent(
-				new CustomEvent('onSitesUpdated', {
-					detail: {
-						sites: resp
-					}
-				})
-			);
-		}
+	    // Dispatch the event
+		document.dispatchEvent( new CustomEvent( "onSitesUpdated", { 
+			"detail": { 
+				sites: resp
+			},
+		}));
+	  }
 	}.bind(this);
 
 	xhr.send();
 };
 
-Ahoy.prototype.update_proxy = function(forceReload) {
+Ahoy.prototype.update_proxy = function ( forceReload ) { 
 	var xhr = new XMLHttpRequest();
 
-	xhr.open('GET', this.api_url + '/api/getProxy');
+	xhr.open("GET", this.api_url + "/api/getProxy" );
 
 	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-			console.log('Got a new Proxy.');
+	  if (xhr.readyState == 4) {
+		 console.log("Got a new Proxy.");
+			  
+	    // JSON.parse does not evaluate the attacker"s scripts.
+	    var resp = JSON.parse(xhr.responseText);
+	    var server = resp.host + ":" + resp.port;
 
-			// JSON.parse does not evaluate the attacker"s scripts.
-			var resp = JSON.parse(xhr.responseText);
-			var server = resp.host + ':' + resp.port;
+	    // Dispatch the event
+	    document.dispatchEvent( new CustomEvent( "onProxyUpdated", { 
+	    	"detail": { 
+	    		proxy_addr: server,
+	    		forceReload: forceReload
+	    	},
+	    }));
 
-			// Dispatch the event
-			document.dispatchEvent(
-				new CustomEvent('onProxyUpdated', {
-					detail: {
-						proxy_addr: server,
-						forceReload: forceReload
-					}
-				})
-			);
-		}
+	  }
 	}.bind(this);
 
 	xhr.send();
@@ -118,246 +116,213 @@ Ahoy.prototype.update_proxy = function(forceReload) {
  * Callbacks
  */
 
-Ahoy.prototype.init_callbacks = function() {
-	console.log('Initializing callbacks');
+Ahoy.prototype.init_callbacks = function( ) {
+	console.log("Initializing callbacks");
 
 	// Setup the callback filters
 	this.setup_callback_filters();
 
 	browser.tabs.onUpdated.addListener(this.update_browse_action_icon.bind(this));
-	browser.webRequest.onResponseStarted.addListener(
-		this.check_for_blocked_site.bind(this),
-		{ urls: ['<all_urls>'] }
-	);
+	browser.webRequest.onResponseStarted.addListener( this.check_for_blocked_site.bind(this), {urls: ["<all_urls>"]} );
 
 	// Stats
-	browser.webNavigation.onCompleted.addListener(this.send_hostname.bind(this), {
-		url: this.webnav_filter_list
-	});
+	browser.webNavigation.onCompleted.addListener( this.send_hostname.bind(this), {url: this.webnav_filter_list } );
 };
 
 Ahoy.prototype.update_callbacks = function() {
 	// Remove all the callbacks
-	console.log('Updating old callbacks...');
+	console.log("Updating old callbacks...");
 
-	browser.tabs.onUpdated.removeListener(
-		this.update_browse_action_icon.bind(this)
-	);
-	browser.webRequest.onResponseStarted.removeListener(
-		this.check_for_blocked_site.bind(this)
-	);
+	browser.tabs.onUpdated.removeListener(this.update_browse_action_icon.bind(this));
+	browser.webRequest.onResponseStarted.removeListener(this.check_for_blocked_site.bind(this));
 
 	// Stats
-	browser.webNavigation.onCompleted.removeListener(
-		this.send_hostname.bind(this)
-	);
+	browser.webNavigation.onCompleted.removeListener(this.send_hostname.bind(this));
 
 	// Recreate new callbacks
 	this.init_callbacks();
-};
+}
 
-Ahoy.prototype.update_browse_action_icon = function(
-	tabId,
-	changeInfo,
-	tabInfo
-) {
-	if (tabInfo.url == undefined) {
+Ahoy.prototype.update_browse_action_icon = function(tabId, changeInfo, tabInfo) {
+	if( tabInfo.url == undefined ) {
 		return;
 	}
 
-	if (!this.is_url_in_list(tabInfo.url)) {
+	if ( ! this.is_url_in_list( tabInfo.url ) ) {
 		return;
 	}
-
+	
 	// Turn the icon red for this tab
 	browser.browserAction.setIcon({
 		path: {
-			'38': 'icons/color/38x38.png'
+			"38":  "icons/color/38x38.png",
 		},
-		tabId: tabId
+		tabId: tabId,
 	});
-};
+}
+
 
 /**
  * If the connection fails, for exemple, dead proxy, get a new one
  */
-Ahoy.prototype.change_proxy_if_connection_fails = function(details) {
-	if (details.error == 'net::ERR_PROXY_CONNECTION_FAILED') {
+Ahoy.prototype.change_proxy_if_connection_fails = function ( details ) {
+	if ( details.error == "net::ERR_PROXY_CONNECTION_FAILED" ) {
 		this.update_proxy();
 	}
 };
 
-Ahoy.prototype.after_update = function(details) {
+Ahoy.prototype.after_update = function( details ) {
 	// Make sure the plugins fetch for new information when it"s installed/updated
 	this.update_site_list();
 	this.update_proxy();
 	console.log(details);
 
 	var last_version = parseVersionString(details.previousVersion);
-	var current_version = parseVersionString(
-		browser.runtime.getManifest().version
-	);
+	var current_version = parseVersionString(browser.runtime.getManifest().version);
 
-	if (
-		last_version.major != current_version.major ||
-		last_version.minor != current_version.minor ||
-		details.reason === 'install'
-	) {
-		browser.tabs.create({
-			url: browser.extension.getURL('views/release_notes.html'),
-			active: true
-		});
+
+	if( last_version.major != current_version.major 
+		|| last_version.minor != current_version.minor 
+		|| details.reason === "install" ) {
+		browser.tabs.create({"url": browser.extension.getURL("views/release_notes.html"), "active": true});
 	}
 };
 
 /**
  * Stats functions
  */
-Ahoy.prototype.send_hostname = function(details) {
-	var parser = document.createElement('a');
-	var hostname = parser.hostname.replace('www.', '');
+Ahoy.prototype.send_hostname = function ( details ) {
+	var parser = document.createElement("a");
+	var hostname = parser.hostname.replace("www.","");
 	var xhr = new XMLHttpRequest();
-
+	
 	parser.href = details.url;
 
-	xhr.open('GET', this.api_url + '/api/stats/host/' + hostname);
+	xhr.open("GET", this.api_url + "/api/stats/host/" + hostname);
 
 	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-			console.log('Stats sent.');
-		}
-	};
+	  if (xhr.readyState == 4) {
+	 	console.log("Stats sent.");
+	  }
+	}
 
 	xhr.send();
 };
 
 Ahoy.prototype.init_events = function() {
 	// Proxy Updated event callback
-	document.addEventListener(
-		'onProxyUpdated',
-		this.event_proxy_updated.bind(this),
-		false
-	);
+	document.addEventListener("onProxyUpdated", this.event_proxy_updated.bind(this), false);
 
 	// Sites list updated event callback
-	document.addEventListener(
-		'onSitesUpdated',
-		this.event_sites_updated.bind(this),
-		false
-	);
+	document.addEventListener("onSitesUpdated", this.event_sites_updated.bind(this), false);
 };
 
-Ahoy.prototype.event_proxy_updated = function(e) {
-	console.log('[EVENT] Proxy updated! New IP = ' + e.detail.proxy_addr);
+Ahoy.prototype.event_proxy_updated = function( e ) {
+	console.log("[EVENT] Proxy updated! New IP = " + e.detail.proxy_addr);
 
 	// Update the fields
-	this.proxy_addr = e.detail.proxy_addr;
-	browser.storage.local.set(
-		{ proxy_addr: e.detail.proxy_addr },
-		function() {
-			// Enable the proxy
-			this.enable_proxy();
-		}.bind(this)
-	);
+ 	this.proxy_addr = e.detail.proxy_addr;
+	browser.storage.local.set( { "proxy_addr": e.detail.proxy_addr }, function() {
+		// Enable the proxy
+		this.enable_proxy();
+	}.bind(this) );
+  	
 };
 
-Ahoy.prototype.event_sites_updated = function(e) {
-	console.log(
-		'[EVENT] Sites list updated. Total size: ' + e.detail.sites.length
-	);
+Ahoy.prototype.event_sites_updated = function( e ) {
+	console.log("[EVENT] Sites list updated. Total size: " + e.detail.sites.length);
 
-	// Update the local storage
-	browser.storage.local.set({ sites_list: e.detail.sites });
+    // Update the local storage
+    browser.storage.local.set( { "sites_list": e.detail.sites } );
 
-	this.sites_list = e.detail.sites;
+    this.sites_list = e.detail.sites;
 
-	// Update the old callbacks
-	this.update_callbacks();
+  	// Update the old callbacks
+  	this.update_callbacks();
 };
 
-Ahoy.prototype.check_for_blocked_redirected_site = function(details) {
+Ahoy.prototype.check_for_blocked_redirected_site = function( details ) {
 	this.check_for_blocked_site(details);
-};
+}
 
-Ahoy.prototype.check_for_blocked_site = function(details) {
+Ahoy.prototype.check_for_blocked_site = function( details ) {
 	// HOTFIX: Fix potential problem with turned off proxy
 	this.enable_proxy();
 
 	// Array with the IP's that the Blocked Page warning usually have.
 	var warning_ips = [
-		'195.23.113.202', // NOS
-		'195.23.113.206', // NOS IGAC
-		'213.13.145.120', // MEO
-		'212.18.182.164', // Vodafone
-		'212.18.182.197', // Vodafone
-		'213.228.128.216', // Cabovisao IGAC
-		'213.228.128.215' // Cabovisao SRIJ
+		"195.23.113.202", 	// NOS
+		"213.13.145.120", 	// MEO
+		"212.18.182.164",	// Vodafone
+		"212.18.182.197",	// Vodafone
+		"213.228.128.216",  // Cabovisao IGAC
+		"213.228.128.215"	// Cabovisao SRIJ
 	];
 
 	// Just bail if this request comes from a redirection.
-	if (this.last_request_redirected === true) {
+	if( this.last_request_redirected === true ) {
 		this.last_request_redirected = false;
 		return;
 	}
 
 	// Ignore all the requests that aren't main
-	if (details.type !== 'main_frame') return;
+	if( details.type !== "main_frame" )
+		return;
 
 	// Ignore if the IP of the site is not the one from above.
-	if (warning_ips.indexOf(details.ip) === -1) return;
+	if( warning_ips.indexOf( details.ip) === -1 )
+		return;
 
 	// Make sure we flag this as a redirect, if it's the case
-	if (details.redirectUrl !== undefined) this.last_request_redirected = true;
+	if( details.redirectUrl !== undefined )
+		this.last_request_redirected = true;
 
 	// Send the async request
 	var xhr = new XMLHttpRequest();
-	var params = 'site=' + details.url;
+	var params = "site=" + details.url;
 
-	xhr.open('POST', this.api_url + '/api/report/blocked', true);
-	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	xhr.open("POST", this.api_url + "/api/report/blocked", true);
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
 	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			console.log('Blocked site reported! ' + details.url);
-		} else if (xhr.status != 200) {
-			console.log('There was an error reporting this Blocked site');
-		}
-	};
+	  if (xhr.readyState == 4 && xhr.status == 200) {
+	 	console.log("Blocked site reported! " + details.url);
+	  } else if ( xhr.status != 200 ) {
+	  	console.log("There was an error reporting this Blocked site");
+	  }
+	}
 
 	xhr.send(params);
-};
+}
 
 Ahoy.prototype.setup_callback_filters = function() {
-	console.log('Setting up callback filters...');
+	console.log("Setting up callback filters...");
 
 	// Create the filter to be used in the onBeforeRequest
 	this.webreq_filter_list = [];
 
-	for (var siteid in this.sites_list) {
+	for ( var siteid in this.sites_list ) {
 		var site = this.sites_list[siteid];
-		this.webreq_filter_list.push('*://' + site + '/*');
-		this.webreq_filter_list.push('*://*.' + site + '/*');
+		this.webreq_filter_list.push( "*://" + site + "/*" );
+		this.webreq_filter_list.push( "*://*." + site + "/*" );
 	}
 
 	// Create the filter to be used in the onComplete and onErrorOccurred listeners
 	this.webnav_filter_list = [];
 
-	for (var siteid in this.sites_list) {
+	for ( var siteid in this.sites_list ) {
 		var site = this.sites_list[siteid];
 
-		this.webnav_filter_list.push({ hostContains: site });
+		this.webnav_filter_list.push( { "hostContains": site } );
 	}
 };
 
-Ahoy.prototype.is_url_in_list = function(url) {
-	var hostname = url
-		.replace('www.', '')
-		.replace(/(http(s?))\:\/\//g, '')
-		.replace('/', '');
+Ahoy.prototype.is_url_in_list = function( url ) {
+	var hostname = url.replace("www.","").replace(/(http(s?))\:\/\//g,"").replace("/","");
 
-	if (this.sites_list.indexOf(hostname) != -1) {
+    if (this.sites_list.indexOf(hostname) != -1) {
 		return true;
 	}
-
-	return false;
-};
+	
+    return false;
+}
--- Firefox/background.js
+++ Firefox/background.js
@@ -1,36 +1,35 @@
 // Initialize the ahoy
-const ahoy = new Ahoy();
-let blockedHosts = [];
-let proxyAddress = '';
+var ahoy = new Ahoy();
+
+// Location of the proxy script, relative to manifest.json
+const proxyScriptURL = "proxy-handler.js";
 
 /**
  * auxiliar functions
  */
-const sleep = (milliseconds) => {
-	const start = new Date().getTime();
-	for (let i = 0; i < 1e7; i++) {
-		if (new Date().getTime() - start > milliseconds) {
-			break;
-		}
-	}
-};
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
+}
 
-const parseVersionString = (str) => {
-	if (typeof str != 'string') {
-		return false;
-	}
-	const x = str.split('.');
-	// parse from string or default to 0 if can't parse
-	const maj = parseInt(x[0]) || 0;
-	const min = parseInt(x[1]) || 0;
-	const pat = parseInt(x[2]) || 0;
+function parseVersionString (str) {
+  if (typeof(str) != "string") { return false; }
+  var x = str.split(".");
+  // parse from string or default to 0 if can't parse
+  var maj = parseInt(x[0]) || 0;
+  var min = parseInt(x[1]) || 0;
+  var pat = parseInt(x[2]) || 0;
 
-	return {
-		major: maj,
-		minor: min,
-		patch: pat,
-	};
-};
+  return {
+      major: maj,
+      minor: min,
+      patch: pat
+  }
+}
 
 /**
  * Alarms - Periodic Tasks
@@ -38,88 +37,71 @@ const parseVersionString = (str) => {
  */
 
 // Create the periodic alarm to fetch new sites
-browser.alarms.create('update_sites_and_proxy', {
-	delayInMinutes: 30,
-	periodInMinutes: 30,
-});
+browser.alarms.create( "update_sites_and_proxy", { delayInMinutes: 30, periodInMinutes: 30 } );
 
 // Handle the alarms
-browser.alarms.onAlarm.addListener(function (alarm) {
-	if (alarm.name == 'update_sites_and_proxy') {
+
+browser.alarms.onAlarm.addListener( function (alarm) {
+	if (alarm.name == "update_sites_and_proxy") {
 		ahoy.update_site_list();
 		ahoy.update_proxy();
 	}
 });
 
 function getPopup() {
-	return browser.extension.getViews({ type: 'popup' });
+    return browser.extension.getViews( { type: "popup" } );
 }
 
 // Default settings. If there is nothing in storage, use these values.
 const defaultSettings = {
-	sites_list: ['thepiratebay.org'],
-	proxy_addr: 'proxy1.ahoy.pro:3127',
-};
-
-// Default list
-browser.runtime.onInstalled.addListener((details) => {
-	blockedHosts = blockedHosts;
-	browser.storage.local.set({
-		blockedHosts: blockedHosts,
-	});
-});
-
-browser.storage.local.get((data) => {
-	if (data.sites_list) {
-		blockedHosts = data.sites_list;
-	}
-	if (data.proxy_addr) {
-		proxyAddress = data.proxy_addr;
-	}
-});
-
-browser.runtime.onMessage.addListener((message) => {
-	browser.storage.local.set({
-		blockedHosts: message.hosts,
-		proxyAddress: `PROXY ${message.proxy}`,
-	});
-
-	blockedHosts = message.hosts;
-	proxyAddress = `PROXY ${message.proxy}`;
-});
-
-browser.storage.onChanged.addListener((changeData) => {
-	if (changeData.sites_list !== undefined) {
-		blockedHosts = changeData.sites_list.newValue;
-	}
-
-	if (changeData.proxy_addr !== undefined) {
-		proxy_addr = changeData.proxy_addr.newValue;
-	}
-});
-
-// Managed the proxy
-browser.proxy.onRequest.addListener(handleProxyRequest, {
-	urls: ['<all_urls>'],
-});
-
-// On the request to open a webpage
-function handleProxyRequest(requestInfo) {
-	const url = new URL(requestInfo.url);
-	const hostname = url.hostname
-		.replace('www.', '')
-		.replace(/(http(s?))\:\/\//g, '')
-		.replace('/', '');
-
-	if (blockedHosts.indexOf(hostname) != -1) {
-		const proxyHost = proxyAddress.split(':')[0];
-		const proxyPort = proxyAddress.split(':')[1];
-		return { type: 'http', host: proxyHost, port: proxyPort };
-	}
-
-	return { type: 'direct' };
+  sites_list: ["thepiratebay.org"],
+  proxy_addr: "proxy1.ahoy.pro:3127"
 }
 
-browser.proxy.onError.addListener((error) => {
-	console.error(`Proxy error: ${error.message}`);
+// Register the proxy script
+browser.proxy.register(proxyScriptURL);
+
+// Log any errors from the proxy script
+browser.proxy.onProxyError.addListener(error => {
+  console.error(`Proxy error: ${error.message}`);
 });
+
+// Initialize the proxy
+function handleInit() {
+  // update the proxy whenever stored settings change
+  browser.storage.onChanged.addListener((newSettings) => {
+    browser.runtime.sendMessage(newSettings.sites_list.newValue, newSettings.proxy_addr.newValue, {toProxyScript: true});
+  });
+
+  // get the current settings, then...
+  browser.storage.local.get()
+    .then((storedSettings) => {
+      // if there are stored settings, update the proxy with them...
+      if (storedSettings.sites_list && storedSettings.proxy_addr) {
+        browser.runtime.sendMessage({hosts: storedSettings.sites_list, proxy: storedSettings.proxy_addr}, {toProxyScript: true});
+      // ...otherwise, initialize storage with the default values
+      } else {
+        browser.storage.local.set(defaultSettings);
+      }
+
+    })
+    .catch(()=> {
+      console.log("Error retrieving stored settings");
+    });
+}
+
+function handleMessage(message, sender) {
+  // only handle messages from the proxy script
+  if (sender.url != browser.extension.getURL(proxyScriptURL)) {
+    return;
+  }
+
+  if (message === "init") {
+    handleInit(message);
+  } else {
+    // after the init message the only other messages are status messages
+    console.log(message);
+  }
+}
+
+browser.runtime.onMessage.addListener(handleMessage);
